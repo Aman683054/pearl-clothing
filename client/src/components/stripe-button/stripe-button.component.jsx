d745a23254fe588210e824c324b2528ce01429de
unknown <aman683054@gmail.com>
2020-06-08 15:42:14 -0400

    added seperate client folder for front end and added server for Stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 
 
@@ -8,8 +9,19 @@ const StripeCheckoutButton = ({price}) => {
     const publishableKey = 'pk_test_ox9gA7sq9ezE8K4RI8WzMMEV006kuSIM5r';
 
     const token = (token) => {
-        console.log(token);
-        alert("Your payment is successful")
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount : priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert("Payment successful")
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(" There was an issue with your payment. Please make sure to use the provided test credit card")
+        })
     }
 
     return(
